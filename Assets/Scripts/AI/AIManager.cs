9cd6c9c44314cf9a9a5f8ec582a1c694e58c4bb0
MBHayes00 <mbh9947@rit.edu>
2021-05-22 17:36:20 -0400

    Add Chase state

--- Assets/Scripts/AI/AIManager.cs
+++ Assets/Scripts/AI/AIManager.cs
@@ -8,10 +8,25 @@ public class AIManager : MonoBehaviour
     
     NavMeshAgent agent;
 
+    //Player information
+    public GameObject player;
+    Transform playerPos;
+    MeshRenderer playerMesh;
+
+    //Animator
+    Animator managerAnimator;
+
+    //Conditions
+    bool playerSpotted;
+
     // Start is called before the first frame update
     void Start()
     {
         agent = gameObject.GetComponent<NavMeshAgent>();
+        playerMesh = player.GetComponent<MeshRenderer>();
+        playerPos = player.transform;
+
+        managerAnimator = gameObject.GetComponent<Animator>();
     }
 
     public void MoveToPosition(Vector3 position)
@@ -22,6 +37,25 @@ public class AIManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //Checks if the manager can see the player
+        if (playerMesh.isVisible)
+        {
+            Debug.DrawRay(transform.position, playerPos.position - transform.position, Color.red);
+
+            RaycastHit hit;
+            Physics.Raycast(transform.position, playerPos.position - transform.position, out hit);
+             
+            if (hit.transform == playerPos)
+            {
+                playerSpotted = true;
+            }
+        }
+
+        //Triggers the animator
+        if (playerSpotted)
+            managerAnimator.SetBool("PlayerSpotted", true);
+        else
+            managerAnimator.SetBool("PlayerSpotted", false);
+
     }
 }
