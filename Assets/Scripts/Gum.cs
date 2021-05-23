6e2b908530ca2794ebf6827481c006c40343c7bd
EricDelmonico <54674769+EricDelmonico@users.noreply.github.com>
2021-05-23 12:39:15 -0400

    Some fixes, win condition, and scenery stuff

--- Assets/Scripts/Gum.cs
+++ Assets/Scripts/Gum.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Gum : Item
 {
     public static bool dropped = false;
+    protected override bool itemDropped => dropped;
     public override void DropItem()
     {
         dropped = true;
--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -4,5 +4,25 @@ using UnityEngine;
 
 public abstract class Item : MonoBehaviour
 {
+    [SerializeField]
+    protected GameObject[] barriersToLift;
+
+    /// <summary>
+    /// Will return the static dropped variable from each Banana/Pork/Gum
+    /// </summary>
+    protected abstract bool itemDropped { get; }
+    
     public abstract void DropItem();
+
+    private void Update()
+    {
+        if (barriersToLift != null && itemDropped)
+        {
+            for (int i = 0; i < barriersToLift.Length; i++)
+            {
+                barriersToLift[i].SetActive(false);
+            }
+            barriersToLift = null;
+        }
+    }
 }
